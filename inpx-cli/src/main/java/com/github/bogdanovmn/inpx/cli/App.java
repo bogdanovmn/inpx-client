3fd4826a7afa006fba3d690ba4dc5dd31d767172
Mikhail N Bogdanov <bogdanovmn@gmail.com>
2023-08-15 21:59:14 +0300

    app entry point refactoring

--- inpx-cli/src/main/java/com/github/bogdanovmn/inpx/cli/App.java
+++ inpx-cli/src/main/java/com/github/bogdanovmn/inpx/cli/App.java
@@ -21,16 +21,18 @@ import static com.github.bogdanovmn.inpx.cli.SearchEngineMethod.SIMPLE;
 
 @Slf4j
 public class App {
-    private static final String CMD_OPTION__INDEX_FILE                 = "index-file";
-    private static final String CMD_OPTION__SEARCH_TITLE_TERM          = "search-title-term";
-    private static final String CMD_OPTION__SEARCH_AUTHOR_TERM         = "search-author-term";
-    private static final String CMD_OPTION__SEARCH_ENGINE              = "search-engine";
-    private static final String CMD_OPTION__ARCHIVE_DIR                = "archive-dir";
-    private static final String CMD_OPTION__SEARCH_ENGINE_URL          = "search-engine-dir";
-    private static final String CMD_OPTION__SEARCH_ENGINE_CREATE_INDEX = "search-engine-create-index";
-    private static final String CMD_OPTION__SEARCH_MAX_RESULTS         = "search-max-results";
-    private static final String CMD_OPTION__EXPORT_BY_ID               = "export-book-by-id";
-    private static final String CMD_OPTION__EXPORT_TO                  = "export-to";
+    private static final String OPT_INDEX_FILE                 = "index-file";
+    private static final String OPT_SEARCH_TITLE_TERM          = "search-title-term";
+    private static final String OPT_SEARCH_AUTHOR_TERM         = "search-author-term";
+    private static final String OPT_SEARCH_ENGINE              = "search-engine";
+    private static final String OPT_ARCHIVE_DIR                = "archive-dir";
+    private static final String OPT_SEARCH_ENGINE_URL          = "search-engine-dir";
+    private static final String OPT_SEARCH_ENGINE_CREATE_INDEX = "search-engine-create-index";
+    private static final String OPT_SEARCH_MAX_RESULTS         = "search-max-results";
+    private static final String OPT_EXPORT_BY_ID               = "export-book-by-id";
+    private static final String OPT_EXPORT_TO                  = "export-to";
+    private static final String OPT_SHOW_STATISTIC             = "show-statistic";
+
 
     private static final int MAX_RESULTS_DEFAULT = 30;
 
@@ -40,47 +42,58 @@ public class App {
             .withJarName("inpx-tool")
             .withDescription("INPX file browser")
 
-            .withArg(CMD_OPTION__INDEX_FILE, "an index file name")
+            .withArg(OPT_INDEX_FILE, "an index file name")
                 .required()
 
-            .withEnumArg(CMD_OPTION__SEARCH_ENGINE, "search engine", SearchEngineMethod.class)
+            .withEnumArg(OPT_SEARCH_ENGINE, "search engine", SearchEngineMethod.class)
                 .withDefault(SIMPLE)
 
-            .withIntArg(CMD_OPTION__SEARCH_MAX_RESULTS, "search max results")
+            .withIntArg(OPT_SEARCH_MAX_RESULTS, "search max results")
                 .withDefault(MAX_RESULTS_DEFAULT)
 
-            .withIntArg(CMD_OPTION__EXPORT_BY_ID, "export FB2 file by id")
+            .withIntArg(OPT_EXPORT_BY_ID, "export FB2 file by id")
                 .requires(
-                    CMD_OPTION__EXPORT_TO,
-                    CMD_OPTION__ARCHIVE_DIR
+                    OPT_EXPORT_TO,
+                    OPT_ARCHIVE_DIR
                 )
-            .withArg(CMD_OPTION__ARCHIVE_DIR, "an archive directory path")
-            .withArg(CMD_OPTION__EXPORT_TO,   "export FB2 file target directory")
+            .withArg(OPT_ARCHIVE_DIR, "an archive directory path")
+            .withArg(OPT_EXPORT_TO,   "export FB2 file target directory")
 
-            .withArg(CMD_OPTION__SEARCH_TITLE_TERM,  "a search query title term")
-            .withArg(CMD_OPTION__SEARCH_AUTHOR_TERM, "a search query author term")
-            .withArg(CMD_OPTION__SEARCH_ENGINE_URL,  "search engine index directory (only for Lucene engine)")
-            .withFlag(CMD_OPTION__SEARCH_ENGINE_CREATE_INDEX, "create search index (only for Lucene engine)")
-                .requires(CMD_OPTION__SEARCH_ENGINE_URL)
+            .withArg(OPT_SEARCH_TITLE_TERM,  "a search query title term")
+            .withArg(OPT_SEARCH_AUTHOR_TERM, "a search query author term")
+            .withArg(OPT_SEARCH_ENGINE_URL,  "search engine index directory (only for Lucene engine)")
+            .withFlag(OPT_SEARCH_ENGINE_CREATE_INDEX, "create search index (only for Lucene engine)")
+                .requires(OPT_SEARCH_ENGINE_URL)
+
+            .withFlag(OPT_SHOW_STATISTIC, "show books statistic")
 
             .withAtLeastOneRequiredOption(
-                CMD_OPTION__EXPORT_BY_ID,
-                CMD_OPTION__SEARCH_AUTHOR_TERM,
-                CMD_OPTION__SEARCH_TITLE_TERM,
-                CMD_OPTION__SEARCH_ENGINE_CREATE_INDEX
+                OPT_EXPORT_BY_ID,
+                OPT_SEARCH_AUTHOR_TERM,
+                OPT_SEARCH_TITLE_TERM,
+                OPT_SEARCH_ENGINE_CREATE_INDEX,
+                OPT_SHOW_STATISTIC
             )
+
             .withEntryPoint(
                 options -> {
-                    InpxFile index = new InpxFile(
-                        options.get(CMD_OPTION__INDEX_FILE)
-                    );
+                    InpxFile booksIndex = new InpxFile(options.get(OPT_INDEX_FILE));
 
-                    if (options.has(CMD_OPTION__EXPORT_BY_ID)) {
+                    if (options.getBool(OPT_SHOW_STATISTIC)) {
+                        showStatistic(booksIndex);
+                    } else if (options.has(OPT_EXPORT_BY_ID)) {
                         exportToFile(options);
-                    } else if (options.has(CMD_OPTION__SEARCH_TITLE_TERM) || options.has(CMD_OPTION__SEARCH_AUTHOR_TERM)) {
-                        searchBooks(options, index);
-                    } else {
-                        showStatistic(index);
+                    } else if (options.getBool(OPT_SEARCH_ENGINE_CREATE_INDEX)) {
+                        createLuceneIndex(
+                            searchEngine(options, booksIndex)
+                        );
+                    } else if (options.has(OPT_SEARCH_TITLE_TERM) || options.has(OPT_SEARCH_AUTHOR_TERM)) {
+                        searchBooks(
+                            searchEngine(options, booksIndex),
+                            SearchQuery.builder()
+                                .author(options.get(OPT_SEARCH_AUTHOR_TERM))
+                                .title(options.get(OPT_SEARCH_TITLE_TERM))
+                            .build());
                     }
                 }
             ).build().run();
@@ -92,60 +105,63 @@ public class App {
         index.printDuplicates();
     }
 
-    private static void searchBooks(ParsedOptions options, InpxFile inpxFile) throws IOException {
-        SearchEngine engine = ((SearchEngineMethod) options.getEnum(CMD_OPTION__SEARCH_ENGINE))
+    private static SearchEngine searchEngine(ParsedOptions options, InpxFile inpxFile) {
+        return ((SearchEngineMethod) options.getEnum(OPT_SEARCH_ENGINE))
             .engineInstance(
                 inpxFile,
                 SearchEngine.Config.builder()
-                    .indexUrl(options.get(CMD_OPTION__SEARCH_ENGINE_URL))
-                    .maxResults(options.getInt(CMD_OPTION__SEARCH_MAX_RESULTS))
-                .build()
+                    .indexUrl(options.get(OPT_SEARCH_ENGINE_URL))
+                    .maxResults(options.getInt(OPT_SEARCH_MAX_RESULTS))
+                    .build()
             );
-        if (engine instanceof LuceneSearchEngine luceneEngine && options.getBool(CMD_OPTION__SEARCH_ENGINE_CREATE_INDEX)) {
-            luceneEngine.createIndex();
-        }
-        SearchQuery query = SearchQuery.builder()
-            .author(options.get(CMD_OPTION__SEARCH_AUTHOR_TERM))
-            .title(options.get(CMD_OPTION__SEARCH_TITLE_TERM))
-        .build();
+    }
 
-        if (query.applicable()) {
-            engine.search(query).collect(
-                    Collectors.groupingBy(InpFileRecord::authors)
-                ).entrySet().stream()
-                .sorted(
-                    Collections.reverseOrder(
-                        Comparator.comparingInt(e -> e.getValue().size())
-                    )
-                )
-                .forEach(e ->
-                    System.out.printf("%s%n\t%s%n",
-                        e.getKey(),
-                        e.getValue().stream()
-                            .sorted(Comparator.comparing(InpFileRecord::title))
-                            .map(book ->
-                                String.format("%7d [%2s %6s] %s %s%n",
-                                    book.fileId(),
-                                    book.lang(),
-                                    new BytesValue(book.fileSize()).shortString(),
-                                    book.title(),
-                                    book.genres()
-                                )
-                            )
-                            .collect(Collectors.joining("\t"))
-                    )
-                );
+    private static void createLuceneIndex(SearchEngine engine) throws IOException {
+        if (engine instanceof LuceneSearchEngine luceneEngine) {
+            luceneEngine.createIndex();
         } else {
+            throw new IllegalArgumentException("Search index creation support is only for Lucene engine");
+        }
+    }
+
+    private static void searchBooks(SearchEngine engine, SearchQuery query) {
+        if (!query.applicable()) {
             throw new IllegalArgumentException("Input search terms are not applicable. Min term length is 3. %s".formatted(query.toString()));
         }
+
+        engine.search(query).collect(
+                Collectors.groupingBy(InpFileRecord::authors)
+            ).entrySet().stream()
+            .sorted(
+                Collections.reverseOrder(
+                    Comparator.comparingInt(e -> e.getValue().size())
+                )
+            )
+            .forEach(e ->
+                System.out.printf("%s%n\t%s%n",
+                    e.getKey(),
+                    e.getValue().stream()
+                        .sorted(Comparator.comparing(InpFileRecord::title))
+                        .map(book ->
+                            String.format("%7d [%2s %6s] %s %s%n",
+                                book.fileId(),
+                                book.lang(),
+                                new BytesValue(book.fileSize()).shortString(),
+                                book.title(),
+                                book.genres()
+                            )
+                        )
+                        .collect(Collectors.joining("\t"))
+                )
+            );
     }
 
     private static void exportToFile(ParsedOptions options) {
         new BookStorage(
-            options.get(CMD_OPTION__ARCHIVE_DIR)
+            options.get(OPT_ARCHIVE_DIR)
         ).export(
-            options.getInt(CMD_OPTION__EXPORT_BY_ID),
-            options.get(CMD_OPTION__EXPORT_TO)
+            options.getInt(OPT_EXPORT_BY_ID),
+            options.get(OPT_EXPORT_TO)
         );
     }
 }
